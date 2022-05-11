6ce059eef6a13a8232a53b1f5f9b670a161c1916
arian soleimanzadeh <Arian@DESKTOP-GOKUOOQ.Zone_Arian.com>
2022-05-11 12:38:45 +0430

    derakhti edits (api set)

--- assets/Derakhti/js/derakhti-panel.js
+++ assets/Derakhti/js/derakhti-panel.js
@@ -1,146 +1,146 @@
-var config = {
-        container: "#custom-colored",
+// var config = {
+//         container: "#custom-colored",
 
-        nodeAlign: "BOTTOM",
+//         nodeAlign: "BOTTOM",
 
-        connectors: {
-            type: 'curve'
-        },
-        node: {
-            HTMLclass: 'nodeExample1'
-        },
+//         connectors: {
+//             type: 'curve'
+//         },
+//         node: {
+//             HTMLclass: 'nodeExample1'
+//         },
 
-    },
-    user1 = {
-        HTMLclass: 'light-gray',
-        text: {
-            title: "arian",
-            name: "",
-        }
-    },
-    user2 = {
-        parent: user1,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Head of Project Office",
-            name: "Name Surname",
-        }
-    },
-    user3 = {
-        parent: user1,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Head of IT Ops",
-            name: "Name Surname",
-        }
-    },
-    user4 = {
-        parent: user2,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Head of Development",
-            name: "Name Surname",
-        }
-    },
-    user5 = {
-        parent: user2,
+//     },
+//     user1 = {
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "arian",
+//             name: "",
+//         }
+//     },
+//     user2 = {
+//         parent: user1,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Head of Project Office",
+//             name: "Name Surname",
+//         }
+//     },
+//     user3 = {
+//         parent: user1,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Head of IT Ops",
+//             name: "Name Surname",
+//         }
+//     },
+//     user4 = {
+//         parent: user2,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Head of Development",
+//             name: "Name Surname",
+//         }
+//     },
+//     user5 = {
+//         parent: user2,
 
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Head of Portfolio",
-            name: "Name Surname",
-        }
-    },
-    user6 = {
-        parent: user3,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Vision",
-            name: "Name Surname",
-        }
-    },
-    user7 = {
-        parent: user3,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Staff / Skills",
-            name: "Name Surname",
-        }
-    },
-    user8 = {
-        parent: user4,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Project Management",
-            name: "Name Surname"
-        }
-    },
-    user9 = {
-        parent: user4,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Application Support",
-            name: "Name Surname"
-        }
-    },
-    user10 = {
-        parent: user5,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Cybersecurity",
-            name: "Name Surname"
-        }
-    },
-    user11 = {
-        parent: user5,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Infrastructure",
-            name: "Name Surname"
-        }
-    },
-    user12 = {
-        parent: user6,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Management Information",
-            name: "Name Surname"
-        }
-    },
-    user13 = {
-        parent: user6,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Service Desk",
-            name: "Name Surname"
-        }
-    },
-    user14 = {
-        parent: user7,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Tech Services",
-            name: "Name Surname"
-        }
-    },
-    user15 = {
-        parent: user7,
-        HTMLclass: 'light-gray',
-        text: {
-            title: "Research & Development",
-            name: "Name Surname"
-        }
-    },
-    chart_config = [
-        config,
-        user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12, user13, user14, user15
-    ];
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Head of Portfolio",
+//             name: "Name Surname",
+//         }
+//     },
+//     user6 = {
+//         parent: user3,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Vision",
+//             name: "Name Surname",
+//         }
+//     },
+//     user7 = {
+//         parent: user3,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Staff / Skills",
+//             name: "Name Surname",
+//         }
+//     },
+//     user8 = {
+//         parent: user4,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Project Management",
+//             name: "Name Surname"
+//         }
+//     },
+//     user9 = {
+//         parent: user4,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Application Support",
+//             name: "Name Surname"
+//         }
+//     },
+//     user10 = {
+//         parent: user5,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Cybersecurity",
+//             name: "Name Surname"
+//         }
+//     },
+//     user11 = {
+//         parent: user5,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Infrastructure",
+//             name: "Name Surname"
+//         }
+//     },
+//     user12 = {
+//         parent: user6,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Management Information",
+//             name: "Name Surname"
+//         }
+//     },
+//     user13 = {
+//         parent: user6,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Service Desk",
+//             name: "Name Surname"
+//         }
+//     },
+//     user14 = {
+//         parent: user7,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Tech Services",
+//             name: "Name Surname"
+//         }
+//     },
+//     user15 = {
+//         parent: user7,
+//         HTMLclass: 'light-gray',
+//         text: {
+//             title: "Research & Development",
+//             name: "Name Surname"
+//         }
+//     },
+//     chart_config = [
+//         config,
+//         user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12, user13, user14, user15
+//     ];
 
-new Treant(chart_config);
+// new Treant(chart_config);
 
-siblingSeparation: 20;
-subTeeSeparation: 20;
-padding: 10;
+// siblingSeparation: 20;
+// subTeeSeparation: 20;
+// padding: 10;
 
 //======================= chaert part ========================
 //======================= chart two ============
@@ -152,10 +152,16 @@ new Chart("myChartTwo", {
     data: {
         labels: xValuesTwo,
         datasets: [{
-            data: [0, 500, 0, 0, 750],
-            borderColor: "#EC6666",
-            fill: false
-        }]
+                data: [0, 500, 0, 0, 750, 0, 0, 0, 0, 0, 0, 0],
+                borderColor: "rgb(155, 155, 255)",
+                fill: false
+            },
+            {
+                data: [0, 200, 0, 0, 500, 0, 0, 0, 0, 0, 0, 0],
+                borderColor: "orange",
+                fill: false
+            }
+        ]
     },
     options: {
         legend: { display: false },
